41e985a0c390394c6aa9c483623cb8f5c201a6e8
Steve-Jones <steve@stevejones.io>
2019-01-27 19:13:24 -0500

    Change chart height

--- src/components/PopChart.js
+++ src/components/PopChart.js
@@ -246,7 +246,7 @@ class PopChart extends Component {
           options={this.state.options}
           series={this.state.series}
           type="line"
-          // height="600px"
+          height="600px"
           width="100%"
         />
         {/* <button onClick={this.onClick}>Change</button> */}
